057dcc495512c64db6fb0c4760758a5897f20f7f
Petr Palata <petr.palata@gmail.com>
2021-06-11 18:07:51 +0200

    Initial Commit

---
+++ I'm just a memer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm just a memer
+//
+//  Created by Petr Palata on 11.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
