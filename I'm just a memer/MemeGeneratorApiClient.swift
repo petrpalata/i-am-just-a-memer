b6b2cd354ad5fbb89f04194d7453a58584604b56
Petr Palata <petr.palata@gmail.com>
2021-06-12 19:53:35 +0200

    Add selected meme label + introduce generateMeme method that takes meme URL

--- I'm just a memer/MemeGeneratorApiClient.swift
+++ I'm just a memer/MemeGeneratorApiClient.swift
@@ -18,20 +18,32 @@ enum MemeGeneratorError: Error {
 class MemeGeneratorApiClient {
     let baseUrl = URLComponents(string: "https://apimeme.com/meme")
     
-    func generateMeme(_ topText: String, bottomText: String) async throws -> (data: UIImage, response: HTTPURLResponse) {
+    func generateMeme(_ memeName: String, topText: String, bottomText: String) async throws -> (data: UIImage, response: HTTPURLResponse) {
         let session = URLSession.shared
-        let memeRequest = try requestFromParams("", topText: topText, bottomText: bottomText)
-        let response = try await session.data(for: memeRequest, delegate: nil)
-        let meme = UIImage(data: response.0)
-        guard let meme = meme else {
-            throw MemeGeneratorError.invalidImage
+            let memeRequest = try requestFromParams(memeName, topText: topText, bottomText: bottomText)
+            let response = try await session.data(for: memeRequest, delegate: nil)
+            let meme = UIImage(data: response.0)
+            guard let meme = meme else {
+                throw MemeGeneratorError.invalidImage
+            }
+            
+            guard let httpResponse = response.1 as? HTTPURLResponse else {
+                throw MemeGeneratorError.invalidResponseType
+            }
+            
+            return (meme, httpResponse)
+    }
+    
+    func generateMeme(_ imageUrl: URL, topText: String, bottomText: String) async throws -> (data: UIImage, response: HTTPURLResponse) {
+        if let memeName = getMemeNameFromImageUrl(imageUrl) {
+            return try await generateMeme(memeName, topText: topText, bottomText: bottomText)
+        } else {
+            return try await generateMeme(topText, bottomText: bottomText)
         }
-        
-        guard let httpResponse = response.1 as? HTTPURLResponse else {
-            throw MemeGeneratorError.invalidResponseType
-        }
-        
-        return (meme, httpResponse)
+    }
+    
+    func generateMeme(_ topText: String, bottomText: String) async throws -> (data: UIImage, response: HTTPURLResponse) {
+        return try await generateMeme("1990s-First-World-Problems", topText: topText, bottomText: bottomText)
     }
     
     private func requestFromParams(_ memeName: String,
@@ -42,7 +54,7 @@ class MemeGeneratorApiClient {
         }
         
         let queryParameters = [
-            URLQueryItem(name: "meme", value: "1990s-First-World-Problems"),
+            URLQueryItem(name: "meme", value: memeName),
             URLQueryItem(name: "top", value: topText),
             URLQueryItem(name: "bottom", value: bottomText)
         ]
@@ -52,5 +64,14 @@ class MemeGeneratorApiClient {
         }
         
         return URLRequest(url: urlFromComponents)
+    }
+
+    private func getMemeNameFromImageUrl(_ imageUrl: URL) -> String? {
+        if let components = URLComponents(url: imageUrl, resolvingAgainstBaseURL: true),
+           let memeQueryParam = components.queryItems?.first(where: { $0.name == "meme" }),
+           let memeName = memeQueryParam.value {
+            return memeName
+        }
+        return nil
     }
 }
--- I'm just a memer/MemeGeneratorViewController.swift
+++ I'm just a memer/MemeGeneratorViewController.swift
@@ -13,18 +13,17 @@ class MemeGeneratorViewController: UIViewController {
     @IBOutlet weak var topTextField: UITextField?
     @IBOutlet weak var bottomTextField: UITextField?
     @IBOutlet weak var apiFetchIndicator: UIActivityIndicatorView?
+    @IBOutlet weak var selectedMemeLabel: UILabel?
 
     let apiClient = MemeGeneratorApiClient()
     
+    var selectedMeme: Meme?
+    
     @IBAction func generateMemePressed() {
-        let topText = topTextField?.text ?? ""
-        let bottomText = bottomTextField?.text ?? ""
-        
         toggleLoading()
         
         async {
-            let apiResponse = try? await apiClient.generateMeme(topText, bottomText: bottomText)
-            guard let apiResponse = apiResponse else {
+           guard let apiResponse = try? await fetchGeneratedMeme() else {
                 print("An error occured during generateMeme call")
                 toggleLoading()
                 return
@@ -47,6 +46,18 @@ class MemeGeneratorViewController: UIViewController {
         present(hostingViewController, animated: true)
     }
     
+    @MainActor
+    private func fetchGeneratedMeme() async throws -> (data: UIImage, response: HTTPURLResponse) {
+        let topText = topTextField?.text ?? ""
+        let bottomText = bottomTextField?.text ?? ""
+        
+        if let selectedMeme = selectedMeme,
+           let imageUrl = selectedMeme.imageUrl {
+            return try await apiClient.generateMeme(imageUrl, topText: topText, bottomText: bottomText)
+        }
+        return try await apiClient.generateMeme(topText, bottomText: bottomText)
+    }
+    
     private func toggleLoading() {
         apiFetchIndicator?.isHidden.toggle()
         memeImageView?.isHidden.toggle()
@@ -55,6 +66,10 @@ class MemeGeneratorViewController: UIViewController {
 
 extension MemeGeneratorViewController: MemeTypeViewModelDelegate {
     func didSelectMeme(_ meme: Meme?) {
+        selectedMeme = meme
+        if let name = meme?.name {
+            selectedMemeLabel?.text = "Meme selected: \(name)"
+        }
         presentedViewController?.dismiss(animated: true, completion: nil)
     }
 }
