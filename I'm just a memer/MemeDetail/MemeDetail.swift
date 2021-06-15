815d134b69a5b7496a3efe4811e38ffca8666f70
Petr Palata <petr.palata@gmail.com>
2021-06-15 19:24:39 +0200

    Implement stub meme generation in MemeDetail

--- I'm just a memer/MemeDetail/MemeDetail.swift
+++ I'm just a memer/MemeDetail/MemeDetail.swift
@@ -11,16 +11,30 @@ struct MemeDetail: View {
     @StateObject var viewModel: MemeDetailViewModel
     
     var body: some View {
-        VStack {
-            ForEach(0..<viewModel.meme.boxCount, id: \.self) { index in
-                TextField("Caption \(index + 1)", text: $viewModel.captionInputs[index])
-                    .padding(8)
-                    .textFieldStyle(.roundedBorder)
+        ScrollView {
+            VStack {
+                if let imageStub = viewModel.memeImageStub {
+                    Image(uiImage: imageStub).resizable().aspectRatio(contentMode: .fit)
+                } else {
+                    ProgressView()
+                }
+                ForEach(0..<viewModel.meme.boxCount, id: \.self) { index in
+                    TextField("Caption \(index + 1)", text: $viewModel.captionInputs[index])
+                        .padding(8)
+                        .textFieldStyle(.roundedBorder)
+                }
+                Button("Generate Meme") {
+                    //                viewModel.generateMeme()
+                }
             }
-            Spacer()
-            Button("Generate Meme") {
-//                viewModel.generateMeme()
+        }
+        .padding(8)
+        .navigationBarHidden(false)
+        .navigationBarTitle(viewModel.meme.name)
+        .onAppear {
+            async {
+                try? await viewModel.generateStubMeme()
             }
-        }.padding(8).navigationBarHidden(false)
+        }
     }
 }
--- I'm just a memer/MemeDetail/MemeDetailViewModel.swift
+++ I'm just a memer/MemeDetail/MemeDetailViewModel.swift
@@ -10,10 +10,22 @@ import SwiftUI
 class MemeDetailViewModel: ObservableObject {
     let meme: Meme
     
+    let imgFlipApiClient = ImgFlipClient()
+    
     @Published var captionInputs: [String]
+    @Published var memeImageStub: UIImage?
     
     init(_ meme: Meme) {
         self.meme = meme
         self.captionInputs = Array(repeating: "", count: meme.boxCount)
+    }
+    
+    func generateStubMeme() async throws -> UIImage {
+        let stubCaptions = (1...meme.boxCount).map { captionIndex in
+            return "Caption \(captionIndex)"
+        }
+        let image = try await imgFlipApiClient.generateMeme(meme, captions: stubCaptions)
+        memeImageStub = image
+        return image
     }
 }
